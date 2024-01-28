67223c941edf56a89369fbaf9e1d4be3f45d3a14
anibalfn <anibalneto29@gmail.com>
2024-01-27 22:15:07 -0300

    Add functions to bring API data

--- js/pokeAPI.js
+++ js/pokeAPI.js
@@ -1,6 +1,36 @@
 const pAPI = {}
 // Essa const vai servir para armazenar os dados advindos das outras funções
 
+function convertAPIdetails(details) {
+    const pokemon = new Pokemon()
+    pokemon.number = details.id
+    pokemon.name = details.name
+    pokemon.weight = details.weight
+    pokemon.height = details.height
+
+    // transforma os valores default nos meus personalizados
+
+    const pokeTypes = details.types.map((pokeTypeSlot) => pokeTypeSlot.type.name)
+    const [type] = pokeTypes
+
+    pokemon.pokeTypes = types;
+    pokemon.type = type;
+
+    // Se os pokémons possuem mais de um tipo, são guardados num array
+
+    pokemon.image = pokeDetail.sprites.other.dream_world.front_default;
+
+    // Consome a img do pokémon da API
+
+    return pokemon;
+}
+
+pAPI.getPokemonDetails = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertAPIdetails)
+}
+
 pAPI.getPokemons = (offset = 0, limit = 10) => {
     const apiURL = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
@@ -12,7 +42,6 @@ pAPI.getPokemons = (offset = 0, limit = 10) => {
         .then((pokemonData => pokemonData))
 
 }
-
 // A função getPokemons é uma propriedade da pAPI, sendo que os parâmetros determinam a quantidade de pokémons na página
 // O return faz um fetch na URL e recebe uma resposta
 //Essa resposta vem em formato json e é solicitado os results
