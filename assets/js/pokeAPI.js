ee3172b175accd26d7ee0e0442ebfabf564fbf86
anibalfn <anibalneto29@gmail.com>
2024-02-03 01:46:18 -0300

    Add functions

---
+++ assets/js/pokeAPI.js
@@ -0,0 +1,34 @@
+class PokeAPI {
+  async convertAPIdetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.name = pokeDetail.name;
+    pokemon.number = pokeDetail.id;
+    pokemon.image = pokeDetail.sprites.other.dream_world.front_default;
+    pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    pokemon.type = pokemon.types[0];
+    return pokemon;
+  }
+
+  async getPokemonDetail(pokemon) {
+    const response = await fetch(pokemon.url);
+    const pokeDetail = await response.json();
+    return this.convertAPIdetailToPokemon(pokeDetail);
+  }
+
+  async getPokemons(offset = 0, limit = 5) {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    const response = await fetch(url);
+    const jsonBody = await response.json();
+    const pokemons = jsonBody.results;
+
+    const detailRequests = pokemons.map(
+      async (pokemon) => await this.getPokemonDetail(pokemon)
+    );
+    const pokemonsDetails = await Promise.all(detailRequests);
+
+    return pokemonsDetails;
+  }
+}
+
+const pokeAPI = new PokeAPI();
